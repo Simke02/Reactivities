d3d336df652bbf001ea7eae8ff1d6121c844c04d
Simke02 <andrijasimovic2002@gmail.com>
2025-11-05 13:46:27 +0100

    End of section 6

--- client/src/feature/activities/form/ActivityForm.tsx
+++ client/src/feature/activities/form/ActivityForm.tsx
@@ -1,15 +1,19 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import type { FormEvent } from "react";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
     activity?: Activity
     closeForm: () => void
-    submitForm: (activity: Activity) => void
 }
 
-export default function ActivityForm({activity, closeForm, submitForm}: Props) {
+export default function ActivityForm({activity, closeForm}: Props) {
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const {updateActivity, createActivity} = useActivities();
+    
+    //Stavili smo da bude async da bi smo sacekali da se izvrsi http zahtev
+    //i onda zatvorili formu sa closeForm
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault(); //Da se ne reload-uje stranica
         
         const formData = new FormData(event.currentTarget);
@@ -19,9 +23,14 @@ export default function ActivityForm({activity, closeForm, submitForm}: Props) {
             data[key] = value;
         });
 
-        if(activity) data.id = activity.id
-
-        submitForm(data as unknown as Activity)
+        if(activity) {
+            data.id = activity.id
+            await updateActivity.mutateAsync(data as unknown as Activity);
+            closeForm();
+        } else {
+            await createActivity.mutateAsync(data as unknown as Activity);
+            closeForm();
+        }
     }
 
   return (
@@ -33,12 +42,23 @@ export default function ActivityForm({activity, closeForm, submitForm}: Props) {
             <TextField name='title' label='Title' defaultValue={activity?.title}/>
             <TextField name='description' label='Description' defaultValue={activity?.description} multiline rows={3}/>
             <TextField name='category' label='Category'defaultValue={activity?.category} />
-            <TextField name='date' label='Date' type="date" defaultValue={activity?.date}/>
+            <TextField name='date' label='Date' type="date"
+                defaultValue={activity?.date
+                    ? new Date(activity.date).toISOString().split('T')[0]
+                    : new Date().toISOString().split('T')[0]
+                }
+            />
             <TextField name='city' label='City'defaultValue={activity?.city} />
             <TextField name='venue' label='Venue'defaultValue={activity?.venue} />
             <Box display='flex' justifyContent='end' gap={3}>
                 <Button onClick={closeForm} color='inherit'>Cancel</Button>
-                <Button type="submit" color='success' variant="contained">Submit</Button>
+                <Button 
+                    type="submit"
+                    color='success'
+                    variant="contained"
+                    //Dok se izvrsava put zahtev dugme ce biti disabled
+                    disabled={updateActivity.isPending || createActivity.isPending}    
+                >Submit</Button>
             </Box>
         </Box>
     </Paper>
---
+++ client/src/lib/hooks/useActivities.ts
@@ -0,0 +1,59 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import agent from "../api/agent";
+
+export const useActivities = () => {
+
+    const queryClient = useQueryClient();
+
+    const {data: activities, isPending} = useQuery({
+        queryKey: ['activities'],
+        queryFn: async () => {
+            const response = await agent.get<Activity[]>('/activities');
+            return response.data;
+        }
+    });
+
+    //Poziva put http zahtev i po uspesnom izvrsavanju
+    //invalidira kes u kome su cuvane aktivnosti
+    //i pribavlja nove podatke od API-ja
+    const updateActivity = useMutation({
+        mutationFn: async (activity: Activity) => {
+            await agent.put('/activities', activity)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    const createActivity = useMutation({
+        mutationFn: async (activity: Activity) => {
+            await agent.post('/activities', activity)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    const deleteActivity = useMutation({
+        mutationFn: async (id: string) => {
+            await agent.delete(`/activities/${id}`)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    return {
+        activities,
+        isPending,
+        updateActivity,
+        createActivity,
+        deleteActivity
+    }
+}
