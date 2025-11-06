807132078786e4ad12788da6ca7166d722dbe913
Simke02 <andrijasimovic2002@gmail.com>
2025-11-06 14:19:35 +0100

    End of section 7

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
 
     const queryClient = useQueryClient();
 
@@ -12,6 +12,16 @@ export const useActivities = () => {
             return response.data;
         }
     });
+
+    const {data: activity, isLoading: isLoadingActivity} = useQuery({
+        queryKey: ['activities', id],
+        queryFn: async () => {
+            const response = await agent.get<Activity>(`/activities/${id}`)
+            return response.data;
+        },
+        //!! castuje id u boolean
+        enabled: !!id
+    })
 
     //Poziva put http zahtev i po uspesnom izvrsavanju
     //invalidira kes u kome su cuvane aktivnosti
@@ -29,7 +39,8 @@ export const useActivities = () => {
 
     const createActivity = useMutation({
         mutationFn: async (activity: Activity) => {
-            await agent.post('/activities', activity)
+            const response = await agent.post('/activities', activity)
+            return response.data;
         },
         onSuccess: async () => {
             await queryClient.invalidateQueries({
@@ -54,6 +65,8 @@ export const useActivities = () => {
         isPending,
         updateActivity,
         createActivity,
-        deleteActivity
+        deleteActivity,
+        activity,
+        isLoadingActivity
     }
 }
